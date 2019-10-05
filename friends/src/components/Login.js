7d6541d07582476863b5f76b4758a30ab5104bb9
benjamin-t-wilson <benjiimanwillson@gmail.com>
2019-10-05 09:37:00 -0700

    built axios functionality into form

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { axiosWithAuth } from "./axiosWithAuth.js";
+import { props } from "bluebird";
 
 const Login = () => {
   const loginObj = {
@@ -6,31 +8,54 @@ const Login = () => {
     password: ""
   };
   const [loginValue, setLoginValue] = useState(loginObj);
+  const [loginStatus, setLoginStatus] = useState(false);
 
   const handleChange = e => {
     setLoginValue({ ...loginValue, [e.target.name]: e.target.value });
   };
 
+  const handleSubmit = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("/login", loginValue)
+      .then(res => {
+        setLoginStatus(true);
+        console.log(res);
+      })
+      .then(() => {
+        setLoginValue(loginObj);
+        props.history.push("/dashboard");
+      })
+      .catch(err => {
+        console.log("Error: ", err);
+      });
+  };
+
   return (
     <section className="login">
-      <form>
-        <label htmlFor="login">Username:</label>
-        <input
-          type="text"
-          name="login"
-          placeholder="LambdaSchoolRox11235"
-          value={loginValue.login}
-          onChange={handleChange}
-        />
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          name="password"
-          placeholder="supersecure"
-          value={loginValue.password}
-          onChange={handleChange}
-        />
-      </form>
+      {!loginStatus ? (
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="login">Username:</label>
+          <input
+            type="text"
+            name="login"
+            placeholder="LambdaSchoolRox11235"
+            value={loginValue.login}
+            onChange={handleChange}
+          />
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="supersecure"
+            value={loginValue.password}
+            onChange={handleChange}
+          />
+          <button type="submit">Login</button>
+        </form>
+      ) : (
+        <p>Logging in...</p>
+      )}
     </section>
   );
 };
